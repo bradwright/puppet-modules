379359fe83ad27a0f6134f378a99d96c9951f740
Bradley Wright <brad@intranation.com>
2012-09-29 18:54:52 +0100

    Make sudoers better behaved for Precise

--- sudo/manifests/init.pp
+++ sudo/manifests/init.pp
@@ -13,8 +13,18 @@ class sudo {
     owner   => root,
     group   => root,
     mode    => 440,
-    source  => "puppet:///modules/sudo/sudoers",
+    source  => "puppet:///modules/sudo/sudoers.${::lsbdistcodename}",
     require => Package['sudo'],
+  }
+
+  if $::lsbdistcodename == 'precise' {
+    file { '/etc/sudoers.d/local':
+      owner   => root,
+      group   => root,
+      mode    => 440,
+      content => "%sudo ALL=(ALL) NOPASSWD: ALL",
+      require => File['/etc/sudoers'],
+    }
   }
 
 }

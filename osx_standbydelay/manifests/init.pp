87035e909d10508bf2ed70be9e3554ac85197fff
Bradley Wright <brad@intranation.com>
2013-01-13 22:33:06 +0000

    Fix variable interpolation

--- osx_standbydelay/manifests/init.pp
+++ osx_standbydelay/manifests/init.pp
@@ -6,7 +6,7 @@ class osx_standbydelay($delay='4200') {
   if $::macosx_productversion_major == '10.8' {
     exec { 'update mountain lion standbydelay':
       command => "pmset -a standbydelay ${delay}",
-      unless  => "pmset -g | grep standbydelay | awk '{print $2}' | grep -Fqe ${version}";
+      unless  => "pmset -g | grep standbydelay | awk '{print $2}' | grep -Fqe ${delay}",
     }
   }
 }
